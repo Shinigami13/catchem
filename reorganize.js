1f934f58fbc1c57ed8b6d972009b4f47f1821c9c
Matthew Rivera <matthewjesseissac@gmail.com>
2018-07-20 14:17:22 -0400

    next i need to input values for stas

---
+++ reorganize.js
@@ -0,0 +1,95 @@
+console.log('linked');
+
+class Pokemon{
+    constructor(name,hp,attack,spattack,defense,spdefense,speed){
+        this.name = name
+        this.hp = hp
+        this.attack = attack
+        this.spattack =spattack
+        this.defense = defense
+        this.spdefense = spdefense
+        this.speed = speed
+        this.abilities = []
+    }
+
+    addAbility(ability){
+        this.abilities.push(ability)
+    }
+}
+
+
+
+// individual class
+class Zetsu{
+    constructor(){
+        this.pokemon = []
+    }
+
+//the all that is supposed to be declared for this project
+    all(){
+        return this.pokemon
+    }
+
+    add(pokemon){
+        this.pokemon.push(pokemon)
+    }
+
+    get(pokemonName){
+        for(i=0;i < this.all.length;i++){
+            if (this.all[i].name === pokemonName){
+                return this.all[i];
+            }
+
+          }
+        }
+};
+
+function loadNinetales() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText)
+        console.log(myObj);
+
+        let pokeName = document.getElementById("ninetales-name")
+        let hp = document.getElementById("ninetales-hp")
+        let attack = document.getElementById("ninetales-attack")
+        let spattack = document.getElementById("ninetales-spattack")
+        let defense = document.getElementById("ninetales-defense")
+        let speed = document.getElementById("ninetales-speed")
+        let abilities = document.getElementById("ninetales-abilities")
+
+
+
+        let ninetales = new Pokemon(
+              myObj.name,
+              myObj.stats[6].base_stat,
+              myObj.stats[5].base_stat,
+              myObj.stats[4].base_stat,
+              myObj.stats[3].base_stat,
+              myObj.stats[2].base_stat,
+              myObj.stats[1].base_stat,
+
+          )
+
+          // data.abilities.forEach((item) => {
+          //     ninetales.addAbility(item.ability.name)
+          //
+          //     abilities.innerHTML += item.ability.name + ", "
+          // })
+
+          // zetsu.add(ninetales)
+          pokeName.innerHTML = ninetales.name
+          hp.innerHTML = ninetales.hp
+          attack.innerHTML = ninetales.attack
+          spattack.innerHTML = ninetales.spattack
+          defense.innerHTML = ninetales.defense
+          spdefense.innerHTML = ninetales.spdefense
+          speed.innerHTML = ninetales.speed
+      }
+    }
+
+      xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/38/", true);
+      xhttp.send();
+
+  };
